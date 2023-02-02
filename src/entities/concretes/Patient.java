c0b9e525bb2939329ec5324fae848e844df6d900
Cemil <okcuoglucemil@gmail.com>
2023-02-02 13:00:47 +0300

    final day

--- src/entities/concretes/Patient.java
+++ src/entities/concretes/Patient.java
@@ -1,4 +1,29 @@
 package entities.concretes;
 
-public class Patient {
+import entities.abstracts.Users;
+import entities.concretes.enums.Gender;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends Users {
+
+    private String tcNo;
+    private LocalDate birthDate;
+    private Gender gender;
+
+    private List<Priority> priorities = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+
+
 }
